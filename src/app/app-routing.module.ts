f0922851c256e339b7031cdb2e5683ef3dd3ed3d
Arturo LR <arturolr@MacBook-Pro-de-Arturo.local>
2022-09-17 14:10:40 +0200

    misc

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
   { path: 'social', loadChildren: () => import('./social/social.module').then(m => m.SocialModule) },
-  { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) }
+  { path: 'resume', loadChildren: () => import('./resume/resume.module').then(m => m.ResumeModule) },
+
+  { path: '**', redirectTo: '/' }
 ];
 
 @NgModule({
