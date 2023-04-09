1ac1bdcd0af19883d347802ab0bb03c5630886b1
Arturo LR <arturolr@MacBook-Pro-de-Arturo.local>
2023-04-09 11:16:20 +0200

    chatbot

--- src/app/chatbot/chatbot.component.ts
+++ src/app/chatbot/chatbot.component.ts
@@ -10,18 +10,14 @@ import { environment } from 'src/environments/environment';
 export class ChatbotComponent {
   messages: any[] = [];
   userMessage = '';
-  conversation: {role: string, content: string}[] = [
-    { role: 'assistant', content: 'Eres el chatbot ayudante de especialista en IT llamado Arturo.' }
-  ];
+  
 
   constructor(private http: HttpClient) {}
 
   sendMessage() {
     this.messages.push({ text: this.userMessage, from: 'sent' });
-    this.conversation.push({ role: 'user', content: this.userMessage });
-    this.http.post<any>(`${environment.api}/chatai`, { messages: this.conversation }).subscribe((res => {
+    this.http.post<any>(`${environment.api}/chatai`, { messages: this.userMessage }).subscribe((res => {
       this.messages.push({ text: res.message, from: 'received' });
-      this.conversation.push({ role: 'assistant', content: res.message });
     }));
 
     this.userMessage = '';
